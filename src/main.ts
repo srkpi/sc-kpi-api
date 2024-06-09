0c7ee41ea800016c2b4328bd366e80eeab8b5e79
Yaroslav Hordiienko <gordienkoyar1@gmail.com>
2024-06-09 16:38:32 +0300

    Fix: added optional port from environment

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import {setup} from "./setup";
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     setup(app);
-    await app.listen(3000);
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
