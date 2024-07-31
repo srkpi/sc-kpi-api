9ef6b7485e3c4d5dbe05e3a485f7e7b145df8e89
Yaroslav Hordiienko <gordienkoyar1@gmail.com>
2024-07-31 20:37:08 +0300

    Removed api key for schedule

--- src/guards/api-key.guard.ts
+++ src/guards/api-key.guard.ts
@@ -7,13 +7,18 @@ import {
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Observable } from 'rxjs';
+import { PUBLIC_KEY } from '../auth/decorators';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
   private readonly logger = new Logger(ApiKeyGuard.name);
   private readonly xApiKey: string;
 
-  constructor(private configService: ConfigService) {
+  constructor(
+    private configService: ConfigService,
+    private reflector: Reflector,
+  ) {
     this.xApiKey = this.configService.get<string>('X_API_KEY');
     if (!this.xApiKey) {
       this.logger.warn('X_API_KEY is missing. Routes are unprotected.');
@@ -22,6 +27,14 @@ export class ApiKeyGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
+
     if (!this.xApiKey) {
       return true;
     }
