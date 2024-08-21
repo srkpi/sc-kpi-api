69c6ce18228a5710a23106ab940602efdb236814
Oleg Zapara <107477650+OlegZapara@users.noreply.github.com>
2024-08-21 22:45:32 +0300

    Switch to docxtemplater library (#41)

--- src/documents/documents.service.ts
+++ src/documents/documents.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { ServiceNoteDto } from './dto/service-note.dto';
-import JSZip from 'jszip';
 import { readFile } from 'fs/promises';
 import { PrismaService } from 'src/prisma/prisma.service';
+import PizZip from 'pizzip';
+import Docxtemplater from 'docxtemplater';
 
 @Injectable()
 export class DocumentsService {
@@ -12,30 +13,36 @@ export class DocumentsService {
     const docxBuffer = await readFile(
       `${__dirname}/templates/service-note.docx`,
     );
-    const zip = new JSZip();
-    const zipContent = await zip.loadAsync(docxBuffer);
-    let documentXml = await zipContent.file('word/document.xml').async('text');
+    const zip = new PizZip(docxBuffer);
+
+    const doc = new Docxtemplater(zip, {
+      paragraphLoop: true,
+      linebreaks: true,
+    });
 
     const result = await this.prismaService.serviceNote.create({ data: dto });
 
-    const replacements = {
-      number: result.id.toString(),
-      receiver: dto.receiver,
-      day: result.createdAt.getDate().toString(),
+    doc.render({
+      number: result.id,
+      day: result.createdAt.getDate(),
       month: this.uaMonth(result.createdAt.getMonth()),
-      year: result.createdAt.getFullYear().toString(),
+      year: result.createdAt.getFullYear(),
+      receiver: dto.receiver,
       title: dto.title,
       content: dto.content,
-    };
+    });
 
-    for (const [placeholder, value] of Object.entries(replacements)) {
-      const placeholderRegex = new RegExp(`\\{\\{${placeholder}\\}\\}`, 'g');
-      documentXml = documentXml.replace(placeholderRegex, value);
-    }
+    const content = doc.getZip().generate({
+      type: 'nodebuffer',
+      compression: 'DEFLATE',
+    });
 
-    zipContent.file('word/document.xml', documentXml);
+    const year = result.createdAt.getFullYear();
+    const shortTitle = dto.title.split(/\s+/).slice(0, 5).join('_');
 
-    return await zipContent.generateAsync({ type: 'nodebuffer' });
+    const name = `Службова_№${result.id}_${year}_${shortTitle}.docx`;
+
+    return { name, content };
   }
 
   private uaMonth(month: number) {
