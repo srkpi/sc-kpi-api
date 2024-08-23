08507327e914921b3ddb5e499384100f2f3a33c7
Oleg Zapara <107477650+OlegZapara@users.noreply.github.com>
2024-08-23 22:38:43 +0300

    Added new club fields (#45)

--- src/clubs/clubs.module.ts
+++ src/clubs/clubs.module.ts
@@ -3,10 +3,11 @@ import { ClubsService } from './clubs.service';
 import { ClubsController } from './clubs.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { ClubProjectsModule } from './projects/projects.module';
+import { ImgurModule } from 'src/imgur/imgur.module';
 
 @Module({
   controllers: [ClubsController],
   providers: [ClubsService],
-  imports: [PrismaModule, ClubProjectsModule],
+  imports: [PrismaModule, ClubProjectsModule, ImgurModule],
 })
 export class ClubsModule {}
