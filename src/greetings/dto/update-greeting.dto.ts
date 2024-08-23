e157a459ddd3f4bc098500324427fee0274fab42
Yaroslav Hordiienko <gordienkoyar1@gmail.com>
2024-08-23 22:48:52 +0300

    Removed redundant endpoints

--- src/greetings/dto/update-greeting.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IntersectionType, PartialType } from '@nestjs/swagger';
-import { CreateGreetingDto } from './create-greeting.dto';
-import { GreetingIdDto } from './greeting-id.dto';
-
-export class UpdateGreetingDto extends IntersectionType(
-  PartialType(CreateGreetingDto),
-  GreetingIdDto,
-) {}
