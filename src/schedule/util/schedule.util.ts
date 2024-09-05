542600de2adeac6d9d778821a0ba0c42a1250e23
Yaroslav Hordiienko <gordienkoyar1@gmail.com>
2024-09-05 19:16:56 +0300

    Fixed timezone bug

--- src/schedule/util/schedule.util.ts
+++ src/schedule/util/schedule.util.ts
@@ -84,7 +84,7 @@ export class ScheduleUtil {
   }
 
   static getISOString(date: Date) {
-    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}T${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.000+02:00`;
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}T${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
   }
 
   static formatDateToUTCString(date: Date, timeZone: string) {
